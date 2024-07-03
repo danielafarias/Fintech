d8c9c028ee2bafad829472039382f0005cf8ccc6
Daniela Farias <daniela.farias@aurum.com.br>
2024-07-03 02:12:29 -0300

    sidebar component

--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -1,7 +1,54 @@
 import React from 'react'
+import resumo from "../assets/icons/resumo.svg";
+import vendas from "../assets/icons/vendas.svg";
+import webhooks from "../assets/icons/webhooks.svg";
+import configuracoes from "../assets/icons/configuracoes.svg";
+import contato from "../assets/icons/contato.svg";
+import sair from "../assets/icons/sair.svg";
+import FintechSVG from '../assets/FintechSVG';
 
 export const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <nav className='sidebar box bg-3'>
+      <FintechSVG title="Fintech logo" />
+      <ul>
+        <li>
+          <span>
+            <img src={resumo} alt="" />
+          </span>
+          <a href="">Resumo</a>
+        </li>
+        <li>
+          <span>
+            <img src={vendas} alt="" />
+          </span>
+          <a href="">Vendas</a>
+        </li>
+        <li>
+          <span>
+            <img src={webhooks} alt="" />
+          </span>
+          <a>Webhooks</a>
+        </li>
+        <li>
+          <span>
+            <img src={configuracoes} alt="" />
+          </span>
+          <a>Configurações</a>
+        </li>
+        <li>
+          <span>
+            <img src={contato} alt="" />
+          </span>
+          <a>Contatos</a>
+        </li>
+        <li>
+          <span>
+            <img src={sair} alt="" />
+          </span>
+          <a>Sair</a>
+        </li>
+      </ul>
+    </nav>
   )
 }
